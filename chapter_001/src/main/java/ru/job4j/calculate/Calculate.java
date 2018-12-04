6e2e4e198b1c6a391afd4630ce54894094f7c77b
John Ivanov <johnivo@mail.ru>
2018-12-04 17:25:37 +0300

    1.3. JavaDoc. Code convention[#92823]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ *@author John Ivanov (johnivo@mail.ru)
+ */
 public class Calculate {
 
+    /**
+	* Main.
+	* @param args 
+	*/
     public static void main (String[] args) {
         System.out.println("Hello World");
     }
