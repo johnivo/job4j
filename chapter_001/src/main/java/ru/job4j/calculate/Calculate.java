5879492b68bd3c1c666267a1b2b02b04f65f09af
John Ivanov <johnivo@mail.ru>
2018-12-06 19:28:27 +0300

    2.5. Инспектор кода IDEA.[#92783]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -11,7 +11,7 @@ public class Calculate {
 
     /**
 	* Main.
-	* @param args 
+	* @param args - args.
 	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
