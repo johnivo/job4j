bd220541028fd4c1cd6fd6d14bd3a48c1766dae0
John Ivanov <johnivo@mail.ru>
2018-12-20 00:32:51 +0300

    Calculate

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,19 +9,19 @@ package ru.job4j.calculate;
  */
 public class Calculate {
 
-    /**
-	* Main.
-	* @param args - args.
-	*/
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 
-/**
-	*Method echo.
-	*@param name Your name.
-	*@return Echo plus your name.
-	*/
+	/**
+	 *Method echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
