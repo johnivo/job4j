8bfa211c4693fb0148acf2a245d91f64a2fa609a
John Ivanov <johnivo@mail.ru>
2019-01-11 23:48:14 +0300

    3. if-else-throw[#92867]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -28,10 +28,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -56,10 +56,9 @@ public class StubInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 }
