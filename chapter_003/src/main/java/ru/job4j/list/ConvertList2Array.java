8cfd4750b12947ecdf9cdbc72ad2dfb500b352cd
John Ivanov <johnivo@mail.ru>
2019-01-27 01:33:09 +0300

    3. Конвертация ArrayList в двухмерный массив[#92849]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,37 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ *@author John Ivanov (johnivo@mail.ru)
+ *@version $Id$
+ *@since 27.01.2018
+ */
+public class ConvertList2Array {
+
+    /**
+     * Метод равномерно разбивает лист на заданное количество строк двумерного массива.
+     *
+     * @param list входящий массив.
+     * @param rows количество строк.
+     * @return двумерный массив.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = list.size() / rows;
+        if (list.size() % rows != 0) {
+            cells++;
+        }
+        int[][] array = new int[rows][cells];
+        Iterator<Integer> it = list.iterator();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cells; j++) {
+                if (it.hasNext()) {
+                    array[i][j] = it.next();
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class.
+ *
+ *@author John Ivanov (johnivo@mail.ru)
+ *@version $Id$
+ *@since 27.01.2019
+ */
+public class ConvertList2ArrayTest {
+
+    @Test
+    public void when7ElementsThen9() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0 ,0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when9ElementsThen10() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 9, 5, 6, 7, 8),
+                2
+        );
+        int[][] expect = {
+                {1, 2, 3, 4, 9},
+                {5, 6, 7, 8, 0},
+        };
+        assertThat(result, is(expect));
+    }
+
+}
