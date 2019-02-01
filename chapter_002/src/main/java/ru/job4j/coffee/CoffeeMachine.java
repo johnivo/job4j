d48d808991961a259022a3c9fd3f64408b35cef6
John Ivanov <johnivo@mail.ru>
2019-02-01 16:18:56 +0300

    CoffeeMachine task

---
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -0,0 +1,66 @@
+package ru.job4j.coffee;
+
+import java.util.Arrays;
+
+/**
+ *
+ *@author John Ivanov (johnivo@mail.ru)
+ *@version $Id$
+ *@since 01.02.2019
+ */
+public class CoffeeMachine {
+
+    /**
+     * Поля: купюра, цена кофе.
+     */
+    private int value;
+    private int price;
+
+    /**
+     * Конструктор.
+     *
+     * @param value купюра.
+     * @param price цена.
+     */
+    public CoffeeMachine(int value, int price) {
+        this.value = value;
+        this.price = price;
+    }
+
+    /**
+     * Метод рассчитывает выдачу сдачи для автомата наименьшим количеством монет.
+     * В автомате монеты номиналом 1, 2, 5, 10.
+     *
+     * @param value купюра 50, 100 и тд.
+     * @param price цена кофе.
+     * @return сдача в виде массива монет.
+     */
+    int[] changes(int value, int price) {
+        int[] result = new int[100];
+        int[] coins = {10, 5, 2, 1};
+        int change = value - price;
+        if (change < 0) {
+            throw new ArithmeticException();
+        }
+        int size = 0;
+        int sum;
+        for (int i = 0; i < coins.length; i++) {
+            if (change > 0 && change % coins[i] != 0) {
+                sum = 0;
+                for (int j = 0; j < (change / coins[i]); j++) {
+                    result[size] = coins[i];
+                    size++;
+                    sum = sum + coins[i];
+                }
+                change = change - sum;
+            } else {
+                for (int j = 0; j < (change / coins[i]); j++) {
+                    result[size] = coins[i];
+                    size++;
+                }
+                break;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/coffee/CoffeeMachineTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.coffee;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class.
+ *
+ *@author John Ivanov (johnivo@mail.ru)
+ *@version $Id$
+ *@since 01.02.2019
+ */
+public class CoffeeMachineTest {
+
+    @Test
+    public void whenValue50ThenChanges15() {
+        int value = 50;
+        int price = 35;
+        CoffeeMachine coffee = new CoffeeMachine(value, price);
+        int[] result = coffee.changes(value, price);
+        int[] expected = {10, 5};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenValue50ThenChanges38() {
+        int value = 50;
+        int price = 12;
+        CoffeeMachine coffee = new CoffeeMachine(value,price);
+        int[] result = coffee.changes(value, price);
+        int[] expected = {10, 10, 10, 5, 2, 1};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenValue100ThenChanges36() {
+        int value = 100;
+        int price = 64;
+        CoffeeMachine coffee = new CoffeeMachine(value,price);
+        int[] result = coffee.changes(value, price);
+        int[] expected = {10, 10, 10, 5, 1};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenValueEqualsPriceThenNoChange() {
+        int value = 50;
+        int price = 50;
+        CoffeeMachine coffee = new CoffeeMachine(value,price);
+        int[] result = coffee.changes(value, price);
+        int[] expected = {};
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void whenValueIsLessThanPriceThenException() {
+        int value = 50;
+        int price = 54;
+        CoffeeMachine coffee = new CoffeeMachine(value,price);
+        int[] result = coffee.changes(value, price);
+    }
+}
