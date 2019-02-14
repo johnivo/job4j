4470eb89bc26b2322ac43b94cc0f0d3bcd45210b
John Ivanov <johnivo@mail.ru>
2019-02-14 21:05:42 +0300

    add class Sorter

--- chapter_003/src/main/java/ru/job4j/sort/Sorter.java
+++ chapter_003/src/main/java/ru/job4j/sort/Sorter.java
@@ -7,20 +7,20 @@ import java.util.TreeSet;
 
 public class Sorter {
 
-    public Sorter(){
+    public Sorter() {
 
     }
 
-    Set<User> sort (List<User> list) {
+    Set<User> sort(List<User> list) {
         TreeSet<User> sortedList = new TreeSet<>();
         sortedList.addAll(list);
         return sortedList;
     }
 
-    public List<User> sortNameLength (List<User> list) {
+    public List<User> sortNameLength(List<User> list) {
         Comparator<User> compar = new Comparator<User>() {
             @Override
-            public int compare (User o1, User o2) {
+            public int compare(User o1, User o2) {
                 return o1.getName().length() - o2.getName().length();
             }
         };
@@ -28,16 +28,16 @@ public class Sorter {
         return list;
     }
 
-    public List<User> sortBoth (List<User> list) {
+    public List<User> sortBoth(List<User> list) {
         Comparator<User> compar1 = new Comparator<User>() {
             @Override
-            public int compare (User o1, User o2) {
+            public int compare(User o1, User o2) {
                 return o1.getName().compareTo(o2.getName());
             }
         };
         Comparator<User> compar2 = new Comparator<User>() {
             @Override
-            public int compare (User o1, User o2) {
+            public int compare(User o1, User o2) {
                 return o1.getAge() - o2.getAge();
             }
         };
