176baa96c841708c448b50b8eeca7566e81e7455
John Ivanov <johnivo@mail.ru>
2019-02-28 16:26:38 +0300

    0. Функциональные итерфейсы. [#92746]

---
+++ chapter_004/src/main/java/ru/job4j/lambda/Calculator.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Calculator {
+    public interface Operation {
+        double calc(int left, int right);
+    }
+
+    public void multiple(int start, int finish, int value,
+                         BiFunction<Integer, Integer, Double> op,
+                         Consumer<Double> media) {
+        for (int index = start; index != finish; index++) {
+            media.accept(op.apply(value, index));
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 2,
+                (value, index) -> {
+                    double result = value * index;
+                    System.out.printf("Multiple %s * %s = %s %n", value, index, result);
+                    return result;
+                },
+                result -> System.out.println(result)
+        );
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void whenAdd1Until3() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = new ArrayList<>();
+        calc.multiple(
+                0, 3, 1,
+                (value, index) -> (double) value + index,
+                result -> buffer.add(result)
+        );
+        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+    }
+}
