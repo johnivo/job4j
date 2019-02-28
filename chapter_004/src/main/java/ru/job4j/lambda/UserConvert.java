9ca464fd9cf75443ce0a54c6cd2c5875b7304bdd
John Ivanov <johnivo@mail.ru>
2019-02-28 21:42:53 +0300

    0.2 Зона видимости в лямбда и исключения[#92748]

--- chapter_004/src/main/java/ru/job4j/lambda/UserConvert.java
+++ chapter_004/src/main/java/ru/job4j/lambda/UserConvert.java
@@ -32,11 +32,29 @@ public class UserConvert {
         return users;
     }
 
-    public static void main(String[] args) {
-        List<String> names = Arrays.asList("Petr", "Nick", "Ban");
-        UserConvert users = new UserConvert();
-        List<User> data = users.convert(names, User::new);
-        data.forEach(System.out::println);
+    public static void badMethod() throws Exception {
+    }
 
+    public static interface Wrapper<T> {
+        T get();
+        void set(T value);
+        boolean isEmpty();
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> names = Arrays.asList("Petr", "Nick", "Ban");
+        Wrapper<Exception> ex = null;
+        names.forEach(
+                n ->  {
+                    try {
+                        badMethod();
+                    } catch (Exception e) {
+                        ex.set(e);
+                    }
+                }
+        );
+        if (!ex.isEmpty()) {
+            throw ex.get();
+        }
     }
 }
