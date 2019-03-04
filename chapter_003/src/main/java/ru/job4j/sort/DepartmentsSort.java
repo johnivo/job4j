9ed9c0cd178997a85a2a0fcae29a62a83c267d75
John Ivanov <johnivo@mail.ru>
2019-03-05 00:47:52 +0300

    Отсортировать департаменты [#34352] //add class Org

--- chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
+++ chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
@@ -10,49 +10,40 @@ import java.util.*;
  * @since 3.03.2019
  */
 public class DepartmentsSort {
-    private List<String> departments;
 
-    public DepartmentsSort(List<String> departments) {
-        this.departments = departments;
+    class Org {
+
+        private List<String> list;
+
+        public Org(List<String> list) {
+            this.list = list;
+        }
+
+        public List<String> getList() {
+            return this.list;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Org org = (Org) o;
+            return this.list.equals(org.list);
+        }
+
+        @Override
+        public int hashCode() {
+            return  Objects.hash(list);
+        }
     }
 
-    public List<String> sortUp(List<String> departments) {
-        departments.sort(
-                new Comparator<String>() {
-                    @Override
-                    public int compare(String o1, String o2) {
-                        return o1.compareTo(o2);
-                    }
-                }
-        );
-        return departments;
-    }
-
-    public List<String> sortDown(List<String> departments) {
-        departments.sort(
-                new Comparator<String>() {
-                    @Override
-                    public int compare(String o1, String o2) {
-                        int result = 0;
-                        int min = Math.min(o1.length(), o2.length());
-                        String a = o1.substring(0, min);
-                        String b = o2.substring(0, min);
-                        for (int i = 0; i < min; i++) {
-                            result = b.compareTo(a);
-                            if (result != 0) {
-                                break;
-                            }
-                        }
-                        return result != 0 ? result : Integer.compare(o1.length(), o2.length());
-                    }
-                }
-        );
-        return departments;
-    }
-
-    public List<String> addDep(List<String> departments) {
-        List<String> allDep = new ArrayList<>(departments);
-        for (String dep : departments) {
+    public List<String> sort(List<String> list, boolean key) {
+        List<String> allDep = new ArrayList<>(list);
+        for (String dep : list) {
             for (int i = 0; i < dep.length(); i++) {
                 if (dep.charAt(i) == '\\') {
                     String headDep = dep.substring(0, i);
@@ -62,6 +53,36 @@ public class DepartmentsSort {
                 }
             }
         }
-        return allDep;
+        Org orgs = new Org(allDep);
+        if (key) {
+            orgs.getList().sort(
+                    new Comparator<String>() {
+                        @Override
+                        public int compare(String o1, String o2) {
+                            return o1.compareTo(o2);
+                        }
+                    }
+            );
+        } else {
+            orgs.getList().sort(
+                    new Comparator<String>() {
+                        @Override
+                        public int compare(String o1, String o2) {
+                            int result = 0;
+                            int min = Math.min(o1.length(), o2.length());
+                            String a = o1.substring(0, min);
+                            String b = o2.substring(0, min);
+                            for (int i = 0; i < min; i++) {
+                                result = b.compareTo(a);
+                                if (result != 0) {
+                                    break;
+                                }
+                            }
+                            return result != 0 ? result : Integer.compare(o1.length(), o2.length());
+                        }
+                    }
+            );
+        }
+        return orgs.getList();
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/DepartmentsSortTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/DepartmentsSortTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertThat;
  *@since 3.03.2019
  */
 public class DepartmentsSortTest {
-    private List<String> departments = new ArrayList<>(
+    private List<String> list = new ArrayList<>(
             Arrays.asList(
                 "K1\\SK1",
                 "K1\\SK2",
@@ -30,18 +30,10 @@ public class DepartmentsSortTest {
             );
 
     @Test
-    public void whenAddDep() {
-        DepartmentsSort sortDep = new DepartmentsSort(departments);
-        int result = sortDep.addDep(departments).size();
-        //List<String> res = sortDep.addDep(departments);
-        //System.out.println(res);
-        assertThat(result, is(9));
-    }
-
-    @Test
     public void whenSortUp() {
-        DepartmentsSort sortDep = new DepartmentsSort(departments);
-        List<String> result = sortDep.sortUp(sortDep.addDep(departments));
+        DepartmentsSort sortDep = new DepartmentsSort();
+        List<String> result = sortDep.sort(list, true);
+        //System.out.println(result);
         List<String> expect = new ArrayList<>();
         expect.addAll(
                 Arrays.asList(
@@ -61,8 +53,8 @@ public class DepartmentsSortTest {
 
     @Test
     public void whenSortDown() {
-        DepartmentsSort sortDep = new DepartmentsSort(departments);
-        List<String> result = sortDep.sortDown(sortDep.addDep(departments));
+        DepartmentsSort sortDep = new DepartmentsSort();
+        List<String> result = sortDep.sort(list, false);
         //System.out.println(result);
         List<String> expect = new ArrayList<>();
         expect.addAll(
@@ -79,24 +71,5 @@ public class DepartmentsSortTest {
                 )
         );
         assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenSortDownTwo() {
-        DepartmentsSort sortDep = new DepartmentsSort(departments);
-        List<String> result = sortDep.sortDown(sortDep.addDep(departments));
-        String[] array = result.toArray(new String[result.size()]);
-        String[] expect = {
-                "K2",
-                "K2\\SK1",
-                "K2\\SK1\\SSK2",
-                "K2\\SK1\\SSK1",
-                "K1",
-                "K1\\SK2",
-                "K1\\SK1",
-                "K1\\SK1\\SSK2",
-                "K1\\SK1\\SSK1"
-        };
-        assertThat(array, is(expect));
     }
 }
