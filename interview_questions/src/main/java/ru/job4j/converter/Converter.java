a5afbac7033b5efe68d3ad35b5befba87438d237
John Ivanov <johnivo@mail.ru>
2019-09-16 00:09:29 +0300

    SQL JDBC interview questions[#126192] up1

--- interview_questions/src/main/java/ru/job4j/converter/Converter.java
+++ interview_questions/src/main/java/ru/job4j/converter/Converter.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class Converter {
 
-    private static final String NAME = "OOD";
+    private static final String NAME = "SQL";
 
     private Integer num = 1;
 
