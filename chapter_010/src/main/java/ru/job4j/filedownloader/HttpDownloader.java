833600c6ce3df7340fd2588eaaa059bfd1a328cd
John Ivanov <johnivo@mail.ru>
2019-10-30 19:32:56 +0300

    1.4. Скачивание файла с ограничением.[#187261]

--- chapter_010/src/main/java/ru/job4j/filedownloader/HttpDownloader.java
+++ chapter_010/src/main/java/ru/job4j/filedownloader/HttpDownloader.java
@@ -3,6 +3,8 @@ package ru.job4j.filedownloader;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * @author John Ivanov (johnivo@mail.ru)
@@ -12,7 +14,7 @@ public class HttpDownloader implements Runnable {
 
     private String url;
     private String target;
-    private  int speedKbSec;
+    private int speedKbSec;
 
     public HttpDownloader(String url, String target, int speedKbSec) {
         this.url = url;
@@ -39,6 +41,7 @@ public class HttpDownloader implements Runnable {
 
                 byte[] buffer = new byte[speedKbSec * 1024];
                 int bytesRead = -1;
+                long start = System.nanoTime();
                 long mark = System.nanoTime();
 
                 while ((bytesRead = reader.read(buffer)) != -1) {
@@ -62,7 +65,17 @@ public class HttpDownloader implements Runnable {
                 writer.close();
                 reader.close();
 
+                long endTime = System.nanoTime();
+
+                long allTime = (endTime - start) / (1000000);  // sec
+                long size = Files.size(Path.of(target)) / 1024;
+                long averageSpeed = (size * 1000) / allTime;
+
                 System.out.println("File saved.");
+
+                System.out.println(String.format("Downloading time %d ms", allTime));
+                System.out.println(String.format("File size %d Kb", size));
+                System.out.println(String.format("Average downloading speed %d Kb/sec", averageSpeed));
 
             } else {
                 System.out.println("Server returned response code " + responseCode + ". Download failed.");
@@ -80,7 +93,7 @@ public class HttpDownloader implements Runnable {
         //String url = "https://docs.oracle.comcom";
         String url = "https://docs.oracle.com/en/java/javase/13/core/java-core-libraries-developer-guide.pdf";
 
-        int speed = 200;
+        int speed = 256;
 
         String target = System.getProperty("java.io.tmpdir")
                 + System.getProperty("file.separator")
