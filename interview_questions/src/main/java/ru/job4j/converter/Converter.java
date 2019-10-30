890dd17b47adb306133b4cf19145784c3a081d83
John Ivanov <johnivo@mail.ru>
2019-10-30 22:58:10 +0300

    add chapter_011 Servlet JSP

--- interview_questions/src/main/java/ru/job4j/converter/Converter.java
+++ interview_questions/src/main/java/ru/job4j/converter/Converter.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class Converter {
 
-    private static final String NAME = "Multithreading";
+    private static final String NAME = "Servlet";
 
     private Integer num = 1;
 
