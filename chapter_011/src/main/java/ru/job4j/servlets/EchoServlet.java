6498dd4ef9de6b7ad986a57a64833cd2e88e45f4
John Ivanov <johnivo@mail.ru>
2019-11-05 22:23:52 +0300

    1. Создать web проект[#187228]

---
+++ chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,23 @@
+package ru.job4j.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author John Ivanov (johnivo@mail.ru)
+ * @since 05.11.2019
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+
+}
